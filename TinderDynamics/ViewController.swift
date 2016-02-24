60111bcba72bffc6fbe26853a361728570dd7628
sashablokhin <sashablokhindev@gmail.com>
2016-02-24 18:04:41 +0300

    Added dynamic behavor to dialog view

--- TinderDynamics/ViewController.swift
+++ TinderDynamics/ViewController.swift
@@ -10,9 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var panRecognizer: UIPanGestureRecognizer!
+    @IBOutlet var dialogView: UIView!
+    
+    var animator: UIDynamicAnimator!
+    var attachmentBehavor: UIAttachmentBehavior!
+    var gravityBehavor: UIGravityBehavior!
+    var snapBehavor: UISnapBehavior?
+    
+    @IBAction func handleGesture(sender: UIPanGestureRecognizer) {
+        let location = sender.locationInView(view)
+        let boxLocation = sender.locationInView(dialogView)
+        
+        if sender.state == .Began {
+            
+            if snapBehavor != nil {
+                animator.removeBehavior(snapBehavor!)
+            }
+            
+            let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(dialogView.bounds), boxLocation.y - CGRectGetMidY(dialogView.bounds))
+            
+            attachmentBehavor = UIAttachmentBehavior(item: dialogView, offsetFromCenter: centerOffset, attachedToAnchor: location)
+            attachmentBehavor.frequency = 0
+            
+            animator.addBehavior(attachmentBehavor)
+            
+        } else if sender.state == .Changed {
+            
+            attachmentBehavor.anchorPoint = location
+            
+        } else if sender.state == .Ended {
+            animator.removeBehavior(attachmentBehavor)
+            
+            snapBehavor = UISnapBehavior(item: dialogView, snapToPoint: view.center)
+            
+            animator.addBehavior(snapBehavor!)
+            
+            let translation = sender.translationInView(view)
+            
+            if translation.y > 100 {
+                animator.removeAllBehaviors()
+                
+                let gravity = UIGravityBehavior(items: [dialogView])
+                gravity.gravityDirection = CGVectorMake(0, 10)
+                
+                animator.addBehavior(gravity)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let scale = CGAffineTransformMakeScale(0.5, 0.5)
+        let translate = CGAffineTransformMakeTranslation(0, -200)
+        
+        dialogView.transform = CGAffineTransformConcat(scale, translate)
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: [], animations: { () -> Void in
+            let scale = CGAffineTransformMakeScale(1, 1)
+            let translate = CGAffineTransformMakeTranslation(0, 0)
+            
+            self.dialogView.transform = CGAffineTransformConcat(scale, translate)
+            }, completion: nil)
+        
+        animator = UIDynamicAnimator(referenceView: view)
     }
 
     override func didReceiveMemoryWarning() {
